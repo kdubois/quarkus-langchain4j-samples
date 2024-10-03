0429b8fcb81761a1770c418317da891164ced065
Kevin Dubois <kevin.dubois@gmail.com>
2024-10-03 07:48:04 +0200

    Add Image Generation example

--- src/main/java/com/redhat/developers/imagegeneration/AiImageGeneration.java
+++ src/main/java/com/redhat/developers/imagegeneration/AiImageGeneration.java
@@ -1,10 +1,16 @@
 package com.redhat.developers.imagegeneration;
 
 import dev.langchain4j.data.image.Image;
-import dev.langchain4j.model.output.Response;
+import dev.langchain4j.service.UserMessage;
+import io.quarkiverse.langchain4j.ModelName;
 import io.quarkiverse.langchain4j.RegisterAiService;
 
-@RegisterAiService(modelName = "image-generation")
-public interface AiImageGeneration {
-    Response<Image> chat (String prompt);
+@RegisterAiService()
+public interface AiImageGeneration { 
+    Image generate(String prompt);
+
+    @UserMessage("""
+            Describe the given message.
+            """)
+    String describe(Image image);
 }
--- src/main/java/com/redhat/developers/imagegeneration/ImageGenerationResource.java
+++ src/main/java/com/redhat/developers/imagegeneration/ImageGenerationResource.java
@@ -1,8 +1,14 @@
 package com.redhat.developers.imagegeneration;
 
+import java.net.URI;
+import java.nio.file.Paths;
+
+import dev.langchain4j.data.image.Image;
+import dev.langchain4j.model.output.Response;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 
 @Path("/image")
@@ -12,7 +18,14 @@ public class ImageGenerationResource {
     AiImageGeneration AI;
 
     @GET
-    public void generateImage(@QueryParam("image") String param) {
-        System.out.println(AI.chat(param).content().url());
+    @Produces("image/png")
+    public java.nio.file.Path def(@QueryParam("prompt") String prompt) {
+        if (prompt == null || prompt.isEmpty()) {
+            throw new IllegalArgumentException("you must provide a \"prompt\" query parameter");
+        }
+        URI fileUri = AI.generate(prompt).url(); // this is saved locally because we have quarkus.langchain4j.openai.image-model.persist=true
+        return Paths.get(fileUri);
     }
+
+    
 }
