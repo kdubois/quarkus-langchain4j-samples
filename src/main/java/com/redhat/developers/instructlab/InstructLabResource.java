0be28d99788c0d16e94bf3414d9ca7b39b024064
Kevin Dubois <kevin.dubois@gmail.com>
2025-03-28 13:16:37 +0100

    update lc4j version

--- src/main/java/com/redhat/developers/instructlab/InstructLabResource.java
+++ src/main/java/com/redhat/developers/instructlab/InstructLabResource.java
@@ -1,10 +1,8 @@
 package com.redhat.developers.instructlab;
 
+import jakarta.enterprise.inject.Default;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/instructlab")
@@ -15,7 +13,10 @@ public class InstructLabResource {
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public String prompt(@QueryParam("message") String message) {
+    public String prompt(
+            @QueryParam("message")
+            @DefaultValue("Create some code that returns a cat image")
+            String message) {
         return assistant.chat(message);
     }
 }
