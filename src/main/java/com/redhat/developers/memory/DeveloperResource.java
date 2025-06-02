226c6bd1e3b0b43f9efbd4ca4451d6098fcaf261
Kevin Dubois <kevin.dubois@gmail.com>
2025-06-02 11:13:32 +0200

    update to quarkus-langchain4j 1.0.0

--- src/main/java/com/redhat/developers/memory/DeveloperResource.java
+++ src/main/java/com/redhat/developers/memory/DeveloperResource.java
@@ -1,28 +1,13 @@
 package com.redhat.developers.memory;
 
-import static dev.langchain4j.data.message.UserMessage.userMessage;
-
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-import dev.langchain4j.chain.ConversationalChain;
-import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.data.message.UserMessage;
-import dev.langchain4j.memory.ChatMemory;
-import dev.langchain4j.memory.chat.TokenWindowChatMemory;
-import dev.langchain4j.model.Tokenizer;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.openai.OpenAiTokenizer;
-import dev.langchain4j.model.output.Response;
-
 @Path("/code")
 public class DeveloperResource {
-
-    @Inject
-    private ChatLanguageModel model;
 
     @Inject
     AssistantWithMemory assistant;
@@ -72,20 +57,16 @@ public class DeveloperResource {
     @Produces(MediaType.TEXT_PLAIN)
     public void generateKubernetes() {
 
-        ConversationalChain chain = ConversationalChain.builder()
-                .chatLanguageModel(model)
-                .build();
-
         String userMessage1 = "Can you give a brief explanation of Kubernetes, 3 lines max?";
         System.out.println("[User]: " + userMessage1 + System.lineSeparator());
 
-        String answer1 = chain.execute(userMessage1);
+        String answer1 = assistant.chat(1, userMessage1);
         System.out.println("[LLM]: " + answer1 + System.lineSeparator());
 
         String userMessage2 = "Can you give me a YAML example to deploy an application for that?";
         System.out.println("[User]: " + userMessage2 + System.lineSeparator());
 
-        String answer2 = chain.execute(userMessage2);
+        String answer2 = assistant.chat(1, userMessage2);
         System.out.println("[LLM]: " + answer2);
 
     }
